d871f4ae28c5e700a5336e306c65900430a99511
marco <marcomerola2001@gmail.com>
2023-11-29 10:49:03 -0400

    change emoji hike

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -41,7 +41,7 @@ const Home = () => (
           I'm a { jobInfo.title }
         </Typography>
         <Typography variant="subtitle1">
-         ⛰ Fueled by hiking
+         🏔️ Fueled by hiking
         </Typography>
 
         <Typography variant="subtitle1">
